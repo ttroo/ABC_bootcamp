7fbf4ea7e62d79bbb8dcb30648bbcc6a3fe203a1
ttroo <kimttroo@gmail.com>
2022-10-20 15:40:08 +0900

    오후 1차 수정을 했다!

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+# from urllib import request
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -9,6 +10,25 @@ def index():
 @app.route('/blog')
 def blog():
     return render_template('blog.html')
+
+@app.route('/test/<username>')
+def test(username):
+    print(username)
+    return render_template('test_result.html', name=username)
+
+@app.route('/methodin')
+def methodin():
+    return render_template('inputform.html')
+
+@app.route('/methodout', methods=['GET', 'POST']) # 기본 값은, methods=['GET']
+def methodout():
+    if request.method == 'POST':
+        print('POST')
+        data = request.form
+    else:
+        print('GET')
+        data = request.args
+    return render_template('method.html', data1 = data, data2 = request.method)
 
 if __name__ == '__main__':
     app.run(debug=True, port=80)
