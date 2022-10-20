36785c99acedb9e175e56507735f644519e7b9de
ttroo <kimttroo@gmail.com>
2022-10-20 11:46:04 +0900

    점심시간 전까지 수정을 해보았다!

--- app.py
+++ app.py
@@ -6,5 +6,11 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
+
 if __name__ == '__main__':
     app.run(debug=True, port=80)
+
+# 수정했으면 Run 해야 함!
